b6c1ece6809dddb1a15721285cf77b33a28e0c30
themanyfaceddemon <neko-d-05@mail.ru>
2025-07-27 00:37:48 +0300

    add

--- data_bases/__init__.py
+++ data_bases/__init__.py
@@ -1,4 +1,4 @@
-from .donate_db import DonateDB, DonateStatus
+from .donate_db import DonateDB, DonateMeta, DonateStatus
 from .log_db import LogDB, LogType
 from .user_db import UserAccess, UserDB
 from .yoomoney_db import YoomoneyDB
--- data_bases/donate_db.py
+++ data_bases/donate_db.py
@@ -1,3 +1,7 @@
+import pickle
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
 from enum import IntFlag
 
 from .base_db import BaseDB
@@ -11,8 +15,70 @@ class DonateStatus(IntFlag):
     NO_STOCK = 1 << 4  # Распродано
 
 
+@dataclass
+class DonateMeta:
+    name: str = ""
+    description: str = ""
+
+    limit: int | None = None
+
+    price_main: Decimal = Decimal(0)
+    sell_time_end: datetime | None = None
+
+    _discount: int = 0
+    _discount_time_end: datetime | None = None
+
+    @property
+    def price(self) -> Decimal:
+        if self._discount <= 0:
+            return self.price_main
+
+        if self._discount >= 100:
+            return Decimal(0)
+
+        return self.price_main * (Decimal(100 - self._discount) / Decimal(100))
+
+    def set_discount(self, discount: int, time_end: datetime | None) -> None:
+        if discount > 100 or discount < 0:
+            raise ValueError("discount bound 0-100")
+
+        if time_end and time_end <= datetime.now():
+            raise ValueError("time_end established in the past")
+
+        self._discount = discount
+        self._discount_time_end = time_end
+
+    def recalculate_discount(self) -> None:
+        if self._discount_time_end is None or self._discount_time_end >= datetime.now():
+            return
+
+        self._discount = 0
+        self._discount_time_end = None
+
+    def is_available(self) -> bool:
+        if self.sell_time_end and self.sell_time_end < datetime.now():
+            return False
+
+        if self.limit is not None and self.limit <= 0:
+            return False
+
+        return True
+
+
 class DonateDB(BaseDB):
     _db_name = "donate"
+
+    @classmethod
+    def pack_meta(cls, data: DonateMeta) -> bytes:
+        return pickle.dumps(data)
+
+    @classmethod
+    def unpack_meta(cls, data: bytes) -> DonateMeta:
+        try:
+            return pickle.loads(data)
+
+        except Exception:
+            return DonateMeta()
 
     @classmethod
     def create_db_table(cls) -> None:
@@ -22,10 +88,25 @@ class DonateDB(BaseDB):
             con.executescript("""
                 CREATE TABLE IF NOT EXISTS services (
                     id TEXT PRIMARY KEY,
-                    price TEXT,
-                    discount INTEGER,
                     meta BLOB,
                     status INTEGER NOT NULL
                 );
             """)
             con.commit()
+
+    @classmethod
+    def get_donates(cls, status: DonateStatus) -> list[dict]:
+        with cls._connect() as con:
+            cur = con.execute(
+                "SELECT * FROM services WHERE (status & ?) != 0", (status,)
+            )
+            rows = cur.fetchall()
+
+        return [
+            {
+                "id": row[0],
+                "meta": cls.unpack_meta(row[1]),
+                "status": DonateStatus(row[2]),
+            }
+            for row in rows
+        ]
