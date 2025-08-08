5b04e2e9e187b422af9a93b2880adc71251fcdb2
themanyfaceddemon <neko-d-05@mail.ru>
2025-08-08 17:51:48 +0300

    мне поебать я полностью рефакторю систему пейментов

--- data_bases/__init__.py
+++ data_bases/__init__.py
@@ -1,11 +1,3 @@
 from .log_db import LogDB, LogType
-from .payment_db import (
-    PaymentCancelReason,
-    PaymentData,
-    PaymentDB,
-    PaymentStatus,
-    ServiceMeta,
-    ServiceStatus,
-)
 from .player_db import PlayerData, PlayerDB
 from .user_db import UserAccess, UserDB
--- main.py
+++ main.py
@@ -8,16 +8,14 @@ from fastapi.routing import APIRoute
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from data_bases import LogDB, PaymentDB, PlayerDB, UserDB
+from data_bases import LogDB, PlayerDB, UserDB
 from data_control import AutoTax, Config, MailControl, ServerControl
 from routers.api.auth import router as api_auth
 from routers.api.discord import router as api_discord
-from routers.api.donate_control import router as api_donate_control
 from routers.api.logs import router as api_logs
 from routers.api.lore_char_control import router as api_lore_char_control
 from routers.api.player_control import router as api_player_control
 from routers.api.server_control import router as api_server_control
-from routers.api.service_control import router as api_service_control
 from routers.api.site_control import router as api_site_control
 from routers.api.steam import router as api_steam
 from routers.api.user_control import router as api_user_control
@@ -42,7 +40,6 @@ async def lifespan(app: FastAPI):
         Config.load()
         LogDB.create_db_table()
         UserDB.create_db_table()
-        PaymentDB.create_db_table()
         PlayerDB.create_db_table()
         AutoTax.setup()
         ServerControl.setup()
@@ -130,12 +127,10 @@ def custom_http_exception_handler(request: Request, exc: StarletteHTTPException)
 
 app.include_router(api_auth, prefix="/api/auth")
 app.include_router(api_discord, prefix="/api/discord")
-app.include_router(api_donate_control, prefix="/api/donate_control")
 app.include_router(api_logs, prefix="/api/logs")
 app.include_router(api_lore_char_control, prefix="/api/lore_char_control")
 app.include_router(api_player_control, prefix="/api/player_control")
 app.include_router(api_server_control, prefix="/api/server_control")
-app.include_router(api_service_control, prefix="/api/service_control")
 app.include_router(api_site_control, prefix="/api/site_control")
 app.include_router(api_steam, prefix="/api/steam")
 app.include_router(api_user_control, prefix="/api/user_control")
