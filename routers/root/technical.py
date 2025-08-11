4baca502ad7603d7f0caeaf696ea5d823ea548d5
themanyfaceddemon <neko-d-05@mail.ru>
2025-08-11 11:47:15 +0300

    update

--- routers/root/technical.py
+++ routers/root/technical.py
@@ -12,3 +12,9 @@ Disallow: /api/
 Disallow: /download/
 """
     return content.strip()
+
+
+@router.get(".well-known/discord", response_class=PlainTextResponse)
+def well_known_discord():
+    content = "dh=60795f3ac4184d38f6942a1963cb65ee9891885c"
+    return content.strip()
