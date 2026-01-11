e91aa0cb672268e41500cbf63752830e1f3133a4
themanyfaceddemon <neko-d-05@mail.ru>
2026-01-12 01:18:41 +0300

    fix bug

--- routers/root/__init__.py
+++ routers/root/__init__.py
@@ -1,8 +1,6 @@
 from include_routers import include_routers_from_package
 
 from .profile import router as _root_profile
-from .wiki.wiki_render import router as _root_wiki
 
 router = include_routers_from_package(__name__)
-router.include_router(_root_wiki)
 router.include_router(_root_profile)
